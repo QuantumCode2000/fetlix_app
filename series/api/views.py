1631644bee73af16949848137e0f3f0ef3db8190
QuantumCode2000 <tancaraninajorgeluis@gmail.com>
2023-07-18 17:48:51 -0400

    utilizando ModelViewSet

--- series/api/views.py
+++ series/api/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from series.models import Serie
 from series.api.serializers import SerieSerializer
-from rest_framework.viewsets import ViewSet
+from rest_framework.viewsets import ViewSet, ModelViewSet
 
 # class SerieApiView(APIView):
 #     def get(self, request):
@@ -22,17 +22,24 @@ from rest_framework.viewsets import ViewSet
 # utilizaremos el viewset
 # los viewset son metodos que se adantan mejor a las apirest
 # con metodos como list,retrieve,create
-class SerieApiView(ViewSet):
-    def list(self, request):
-        series = SerieSerializer(Serie.objects.all(),many=True)
-        return Response(data=series.data, status=status.HTTP_200_OK)
-    def retrieve(self,request, pk:int):
-        series = SerieSerializer(Serie.objects.get(pk=pk))
-        return Response(data=series.data, status=status.HTTP_200_OK)    
-    def create(self, request):
-        serie_serializer = SerieSerializer(data=request.POST)
-        serie_serializer.is_valid(raise_exception=True)
-        Serie.objects.create(title=serie_serializer.validated_data['title'], description=request.POST['description'])
-        return self.list(request)
+# class SerieApiView(ViewSet):
+#     def list(self, request):
+#         series = SerieSerializer(Serie.objects.all(),many=True)
+#         return Response(data=series.data, status=status.HTTP_200_OK)
+#     def retrieve(self,request, pk:int):
+#         series = SerieSerializer(Serie.objects.get(pk=pk))
+#         return Response(data=series.data, status=status.HTTP_200_OK)    
+#     def create(self, request):
+#         serie_serializer = SerieSerializer(data=request.POST)
+#         serie_serializer.is_valid(raise_exception=True)
+#         Serie.objects.create(title=serie_serializer.validated_data['title'], description=request.POST['description'])
+#         return self.list(request)
 #! este viewset debe anadirse a un router 
 #! un router es un componente que es capaz de entender las peticiones al metodo que nos conviene
+
+# ? UTILIZANDO LOS MODELVIEWSET
+class SerieApiView(ModelViewSet):
+    queryset = Serie.objects.all()
+    serializer_class = SerieSerializer
+    
+    
