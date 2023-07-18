800198bf018f007d7e9c9c3f54717cb2db9295de
QuantumCode2000 <tancaraninajorgeluis@gmail.com>
2023-07-17 20:22:16 -0400

    soluciones errores de clean code

--- series/views.py
+++ series/views.py
@@ -48,10 +48,9 @@ class SerieView(View):
         return redirect('login')
     
 # documentacion de los mixin
-"""
-! La siguiente clase EpisodeView(View) es igual a la 
-! clase EpisodeView(LoginRequiredMixin,View)
-"""
+
+# ! La siguiente clase EpisodeView(View) es igual a la ==>
+# ! clase EpisodeView(LoginRequiredMixin,View)
 
 # class EpisodeView(View):
 #     """
@@ -59,18 +58,18 @@ class SerieView(View):
 #     """
 #     def get(self, request, serie_id : int ):
 #         """
-#         get 
+#         get
 #         """
-#         if request.user.is_authenticated:    
+#         if request.user.is_authenticated:
 #             context = {
 #                 'episodes':list(Episode.objects.filter(serie_id=serie_id))
 #             }
 #             return render(request,'episodes.html',context=context)
 #         return redirect('login')
-"""
-la siguiente clase es la clase que nos hara lo mismo que si
-agregamos una condicianal dentro de la clase para ejecutar la misma
-"""
+
+# la siguiente clase es la clase que nos hara lo mismo que si ==>
+# agregamos una condicianal dentro de la clase para ejecutar la misma
+
 class EpisodeView(LoginRequiredMixin,View):
     """
     Esta clase con el parametro LoginRequiredMixin es igual al poner
