0ffed8d50c5f1c2842934d29d6a20a67125282d2
QuantumCode2000 <tancaraninajorgeluis@gmail.com>
2023-07-17 23:03:32 -0400

    anadiendo los modelos de Serie y Episode a Django Admin

--- series/admin.py
+++ series/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+# from django.contrib.admin import ModelAdmin
+from series.models import Serie,Episode
+# from django.contrib.admin.decorators import register
 # Register your models here.
+# class SeriesAdmin(ModelAdmin):
+#     pass
+
+admin.site.register(Serie)
+admin.site.register(Episode)
+
+
+# @register(Episode)
+# class EpidoseAdmin(ModelAdmin):
+#     pass
